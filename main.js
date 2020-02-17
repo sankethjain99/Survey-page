71b590420f2e3585e8e88e3bef419fefa811c4c0
Sanketh s jain <jsanketh1799@gmail.com>
2020-02-17 20:04:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCC9zs7SounFzuTzsAGfWe-MZyvY69KVHs",
+    authDomain: "survey-96acf.firebaseapp.com",
+    databaseURL: "https://survey-96acf.firebaseio.com",
+    projectId: "survey-96acf",
+    storageBucket: "survey-96acf.appspot.com",
+    messagingSenderId: "175505794456",
+    appId: "1:175505794456:web:172a3912d28ebef978b7ed",
+    measurementId: "G-532B45VVV1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
